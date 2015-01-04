27a8635337c8215bfd18729f17e610d24e98d0f8
Gilles Cornu <git@gilles.cornu.name>
2015-01-05 00:42:34 +0100

    Fix Travis install stage on linux

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -4,7 +4,7 @@
 
 echo "Install Ansible"
 sudo apt-get install --yes --quiet software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt-get update -qq
 sudo apt-get install --yes --quiet ansible
 
