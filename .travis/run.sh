71dd547b483b120caca73f9574027b876f8cad40
Gilles Cornu <git@gilles.cornu.name>
2015-01-08 00:44:21 +0100

    Fix syntax error in Travis CI main build script

--- .travis/run.sh
+++ .travis/run.sh
@@ -25,7 +25,7 @@ echo "rubber testing stuff is pending..."
 cd $TRAVIS_BUILD_DIR
 
 echo "Customize dispansible settings for Travis"
-cp .travis/settings_{TRAVIS_OS_NAME}.yml ./settings.yml
+cp .travis/settings_${TRAVIS_OS_NAME}.yml ./settings.yml
 
 echo "Run dispansible"
 ./dispansible all
