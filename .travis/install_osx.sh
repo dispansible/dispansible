38295cbdf88ce07a83490401a2843b7321a88d6b
Gilles Cornu <git@gilles.cornu.name>
2015-01-08 10:45:09 +0100

    Reuse travis setup scripts as bootstrapper scripts

--- .travis/install_osx.sh
+++ .travis/install_osx.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Install Dependencies
-
-echo "Install Ansible"
-brew update
-brew install ansible
-
-echo "Install ansible-lint tool"
-sudo pip install ansible-lint
+../scripts/bootstrap_macosx.sh
---
+++ scripts/bootstrap_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Dependencies
+
+echo "Install Ansible"
+sudo apt-get install --yes --quiet software-properties-common
+sudo apt-add-repository --yes ppa:ansible/ansible
+sudo apt-get update -qq
+sudo apt-get install --yes --quiet ansible
+
+echo "Install ansible-lint tool"
+sudo pip install ansible-lint
