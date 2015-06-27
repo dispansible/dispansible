6b5f49fd09fe56d803f085d4bba5b1c9dbd413db
Gilles Cornu <git@gilles.cornu.name>
2015-06-27 02:03:27 +0200

    = travis-ci: fix a typo in the run.sh script

--- tests/travis/run.sh
+++ tests/travis/run.sh
@@ -15,10 +15,10 @@ ansible-lint $PLAYBOOK
 
 echo "Run ansible syntax checks"
 # use any inventory (macosx or linux) with localhost as single machine
-ansible-playbook --inventory-file=inventories/macosx --syntax-check $PLAYBOOK"
+ansible-playbook --inventory-file=inventories/macosx --syntax-check $PLAYBOOK
 
 echo "Customize dispansible settings for Travis"
-export DISPANSIBLE_SETTINGS_DIR=${TRAVIS_BUILD_DIR}/tests/travis/${TRAVIS_OS_NAME}
+export DISPANSIBLE_SETTINGS_DIR="${TRAVIS_BUILD_DIR}/tests/travis/${TRAVIS_OS_NAME}"
 
 echo "Run dispansible"
 cd $TRAVIS_BUILD_DIR
